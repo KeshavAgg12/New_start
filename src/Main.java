a6bad724f6444b5269dd628350690b8ede376a07
KeshavAgg12 <aggarwalkeshav250@gmail.com>
2025-08-08 18:00:09 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
+import java.util.Calendar;
+public class Exercise1 {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the marks of following subjects to get percentage: a");
+        System.out.print("Maths:");
+        float maths= sc.nextFloat();
+        System.out.print("physics:");
+        float phy= sc.nextFloat();
+        System.out.print("chemistry:");
+        float chem=sc.nextFloat();
+        System.out.print("English:");
+        float eng=sc.nextFloat();
+        System.out.print("programming:");
+        float progm=sc.nextFloat();
+        float total=maths+phy+chem+progm+eng;
+        System.out.print("Total is:");
+        System.out.println(total);
+        float per=total/5;
+        System.out.print("Percentage is :");
+        System.out.print(per+"%");
     }
 }
